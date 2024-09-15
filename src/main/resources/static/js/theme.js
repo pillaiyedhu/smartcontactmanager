ef2bb0a6262244f28f88c58254879720f8750659
pillaiyedhu <pillaiyedhu@gmail.com>
2024-09-15 10:33:03 +0530

    update : dark and light theme

---
+++ src/main/resources/static/js/theme.js
@@ -0,0 +1,45 @@
+let currentTheme = getTheme();
+
+changeTheme();
+
+function changeTheme(){
+    changePageTheme(currentTheme,"");
+    const themeButton = document.querySelector("#theme_button");
+    //console.log(themeButton);
+
+    themeButton.addEventListener("click",(event)=>{
+        let oldTheme = currentTheme;
+        if(currentTheme=="dark"){
+            currentTheme="light";
+        }
+        else{
+            currentTheme="dark";
+        }
+        //console.log(currentTheme);
+        changePageTheme(currentTheme,oldTheme);
+    });
+}
+
+function changePageTheme(theme,oldTheme){
+    setTheme(currentTheme);
+
+    if(oldTheme){
+        document.querySelector('html').classList.remove(oldTheme);
+    }
+    document.querySelector('html').classList.add(theme);
+
+    document.querySelector("#theme_button").querySelector("span").textContent = theme == "light" ? "Dark" : "Light";
+
+}
+
+
+function setTheme(theme){
+    localStorage.setItem("theme",theme);
+}
+
+
+function getTheme(){
+    let theme = localStorage.getItem("theme");
+    console.log(theme);
+    return theme ? theme : "light";
+}
